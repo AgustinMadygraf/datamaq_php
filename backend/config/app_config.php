d70cadfd5b7636d93863a20eb3b6ea8217b30888
AgustinMadygraf <agustin.mtto.madygraf@gmail.com>
2025-08-20 15:15:52 -0300

    refactor(env): Replace example.env with env.example.php for environment variable configuration

--- backend/config/app_config.php
+++ backend/config/app_config.php
@@ -1,8 +1,6 @@
 <?php
-require_once __DIR__ . '/../../vendor/autoload.php';
+require_once __DIR__ . '/../../env.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
-$dotenv->load();
 
 // Database configuration
 define('DB_SERVER', $_ENV['DB_SERVER']);
