46a2dc0030e196477a66b4071f5ff20410d5bae5
AgustinMadygraf <agustin.mtto.madygraf>
2025-09-06 22:21:47 -0300

    refactor(api): Remove deprecated API components and clean up unused files

--- backend/api/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-/*
-Path: backend/api/index.php
-*/
-
-header('Content-Type: application/json; charset=utf-8');
-require_once __DIR__ . '/Router.php';
--- backend/views/header.php
+++
@@ -1,4 +0,0 @@
-<?php 
-require_once __DIR__ . '/../core/ViewRenderer.php';
-echo ViewRenderer::render('/datamaq/templates/header.html');
-?>
